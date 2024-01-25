bcbc3da5511d722cc275413ca9796e71fb7d664b
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-01-26 08:00:51 +0900

    Add chapter6

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // mod chapter1;
 // mod chapter2;
 // mod chapter4;
-mod chapter5;
+// mod chapter5;
+mod chapter6;
 
 fn main() {
     // chapter1::thread_use();
     // chapter2::atomic();
     // chapter4::do_chapter();
-    chapter5::do_channel();
+    // chapter5::do_channel();
+    chapter6::do_arc();
 }
